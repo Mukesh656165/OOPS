953f04d0b4d323c2f5e5e29bcd295855aaea50a9
MUKESH <mukesh.ora@outlook.com>
2020-01-19 02:20:42 +0530

    lines added

--- Class.py
+++ Class.py
@@ -1,1 +1,15 @@
+# Creating a simple class
+class SimpleClass:
+    '''This is indicating the simple class'''
+    def __init__(self):
+        self.a = 10
+        self.name = 'Python_class'
+
+'''creating class object'''
+s = SimpleClass()
+'''To check the instance variable present inside class'''
+print(s.__dict__)
+
+
+
 
